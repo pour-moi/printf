2b0f0d119832276702002e74c7af7efaa10cfcfb
pour-moi <pour64moi@gmail.com>
2023-08-20 23:56:44 +0300

    prints text

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _printf - printf c and s format speciefiers
+* @format: modulo
+*
+* Return: count Success
+*/
+
+
+int _printf(const char *format, ...)
+{
+	va_list arguments;
+	int count = 0;
+
+	va_start(arguments, format);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char arg = va_arg(arguments, int);
+
+				count += write(1, &arg, 1);
+			}
+			if (*format == 's')
+			{
+				char *str = va_arg(arguments, char *);
+
+				count += write(1, str, strlen(str));
+			}
+		}
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	va_end(arguments);
+	return (count);
+}
