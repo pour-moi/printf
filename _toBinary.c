e4401ba2a1f17d29ddb096628fcbec588c344ee0
pour-moi <pour64moi@gmail.com>
2023-08-21 16:21:40 +0300

    Deleted the _printf function

--- _toBinary.c
+++ _toBinary.c
@@ -7,10 +7,10 @@
  *
  * Return: binary number
  */
-int _binary(va_list arguments)
+int* _binary(va_list arguments)
 {
 	int binary = va_arg(arguments, int);
-	int bin[32];
+	static int bin[32];
 	int i = 0;
 
 	while (binary > 0)
@@ -21,50 +21,7 @@ int _binary(va_list arguments)
 	while (i > 0)
 	{
 		i--;
-		return (write(1, &bin[i], 1));
+		(write(1, &bin[i], 1));
 	}
-}
-
-/**
-* _printf - outputs texts
-* @format: modulo
-*
-* Return: size
-*/
-
-int _printf(const char *format, ...)
-{
-	va_list arguments;
-	int count = 0;
-
-	va_start(arguments, format);
-	if (format == NULL)
-		return (-1);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0')
-				return (-1);
-			if (*format == 'b')
-			{
-				count += _binary(arguments);
-			}
-			else if (*format == '%')
-			{
-				count += write(1, format, 1);
-			}
-			else
-			{
-				count += write(1, "%", 1);
-				count += write(1, format, 1);
-			}
-		}
-		else
-			count += write(1, format, 1);
-		format++;
-	}
-	va_end(arguments);
-	return (count);
+	return (bin);
 }
